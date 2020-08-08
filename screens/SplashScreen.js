a8ff2a227cb8d9e088e57bcc586acbf37918567b
CastalJaemin <ehdrud1332@gmail.com>
2020-08-08 10:59:11 +0900

    LogoBounce & Gradient Button

---
+++ screens/SplashScreen.js
@@ -0,0 +1,107 @@
+import React from 'react';
+import {
+    View,
+    Text,
+    TouchableOpacity,
+    Dimensions,
+    StyleSheet,
+    Image,
+    Button
+} from 'react-native';
+import * as Animatable from 'react-native-animatable';
+import {LinearGradient} from 'expo-linear-gradient';
+import {Ionicons} from '@expo/vector-icons';
+
+
+const SplashScreen = ({navigation}) => {
+    return (
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <Animatable.Image
+                    animation="bounceIn"
+                    duration="1500"
+                    source={require('../assets/png.png')}
+                    style={styles.logo}
+                    resizeMode="stretch"
+                />
+            </View>
+            <Animatable.View
+                style={styles.footer}
+                animation="fadeInUpBig"
+            >
+                <Text style={styles.title}>Stay connect with everyone</Text>
+                <Text style={styles.text}>Sign in with account</Text>
+                <View style={styles.button}>
+                    <TouchableOpacity onpress={() => navigation.navigate("SignInScreen")}>
+                        <LinearGradient
+                            colors={['#08d4c4', '#01ab9d']}
+                            style={styles.signIn}
+                        >
+                            <Text style={styles.textSign}>Get Started     </Text>
+                            <Ionicons
+                                name="ios-arrow-forward"
+                                color="#fff"
+                                size={20}
+                            />
+                        </LinearGradient>
+                    </TouchableOpacity>
+                </View>
+            </Animatable.View>
+        </View>
+
+    );
+};
+
+const {height} = Dimensions.get("screen");
+const height_logo = height * 0.28;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#009387'
+    },
+    header: {
+        flex: 2,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    footer: {
+        flex: 1,
+        backgroundColor: '#fff',
+        borderTopLeftRadius: 30,
+        borderTopRightRadius: 30,
+        paddingVertical: 50,
+        paddingHorizontal: 30
+    },
+    logo: {
+        width: height_logo,
+        height: height_logo
+    },
+    title: {
+        color: '#05375a',
+        fontSize: 30,
+        fontWeight: 'bold'
+    },
+    text: {
+        color: 'grey',
+        marginTop:5
+    },
+    button: {
+        alignItems: 'flex-end',
+        marginTop: 30
+    },
+    signIn: {
+        width: 150,
+        height: 40,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 20,
+        flexDirection: 'row'
+    },
+    textSign: {
+        color: 'white',
+        fontWeight: 'bold'
+    }
+});
+
+export default SplashScreen;
